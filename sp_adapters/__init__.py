65402649aeda13fc6e53cf9c71db4c01cb639b32
LukasHedegaard <lh@eng.au.dk>
2022-09-28 09:51:45 +0200

    Update __init__

--- sp_adapters/__init__.py
+++ sp_adapters/__init__.py
@@ -1,1 +1,12 @@
-from .splopa import SPLoPA, SPLoPAdapter, SPLoPALinear  # noqa: F401
+from .lora import LowRankMatrix
+from .splopa import SPLoPA, SPLoPALinear
+
+# from .splora import SPLoRA, SPLoRALinear
+
+__all__ = [
+    "SPLoPA",
+    "SPLoPALinear",
+    # "SPLoRA",
+    # "SPLoRALinear",
+    "LowRankMatrix",
+]
