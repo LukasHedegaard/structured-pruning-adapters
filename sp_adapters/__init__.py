5a0665ed94f5150e7c1bf69f8bae499b55c0a4f7
LukasHedegaard <lh@eng.au.dk>
2022-10-19 09:58:29 +0200

    Add SPLoRA convs to __init__and bump version

--- sp_adapters/__init__.py
+++ sp_adapters/__init__.py
@@ -1,11 +1,14 @@
 from .lora import LowRankMatrix
 from .splopa import SPLoPA, SPLoPALinear
-from .splora import SPLoRA, SPLoRALinear
+from .splora import SPLoRA, SPLoRALinear, SPLoRAConv1d, SPLoRAConv2d, SPLoRAConv3d
 
 __all__ = [
     "SPLoPA",
     "SPLoPALinear",
     "SPLoRA",
     "SPLoRALinear",
+    "SPLoRAConv1d",
+    "SPLoRAConv2d",
+    "SPLoRAConv3d",
     "LowRankMatrix",
 ]
