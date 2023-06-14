6e3380dc6085bb6497a0f3fcbf375339bc835ea6
LukasHedegaard <lh@eng.au.dk>
2023-06-14 09:35:48 +0200

    Add SPLoRAMultiheadAttention

--- tests/test_splora.py
+++ tests/test_splora.py
@@ -167,6 +167,231 @@ def test_splora_conv():
             assert torch.equal(conv2.bias, spconv.bias)
 
 
+def test_splora_mha_equal_qkv_dim():
+    rank = 2
+    embed_dim = 16
+    num_heads = 4
+    seq_len = 8
+    x = torch.randn(seq_len, embed_dim)
+    y = torch.randn(seq_len, embed_dim)
+
+    mha = nn.MultiheadAttention(
+        embed_dim,
+        num_heads,
+        dropout=0.0,
+        bias=True,
+        add_bias_kv=False,
+        add_zero_attn=False,
+        kdim=None,
+        vdim=None,
+        batch_first=False,
+    )
+    mha.in_proj_bias = torch.nn.Parameter(torch.rand_like(mha.in_proj_bias))
+
+    # Transfer
+    spmha = SPLoRA(mha, rank)
+
+    # Weights are all close, biases are equal
+    assert torch.equal(
+        mha.in_proj_weight,
+        torch.cat(
+            (
+                spmha.q_proj.weight,
+                spmha.k_proj.weight,
+                spmha.v_proj.weight,
+            )
+        ),
+    )
+    assert torch.allclose(  # in_proj_weight property uses adapted_weights
+        mha.in_proj_weight, spmha.in_proj_weight, atol=_DEFAULT_INIT_RANGE
+    )
+    assert torch.equal(mha.in_proj_bias, spmha.in_proj_bias)
+    assert torch.allclose(
+        mha.out_proj.weight, spmha.out_proj.adapted_weight, atol=_DEFAULT_INIT_RANGE
+    )
+    assert torch.equal(mha.out_proj.bias, spmha.out_proj.bias)
+
+    # Prepare optim
+    optimizer = torch.optim.Adam(spmha.parameters())
+    optimizer.zero_grad()
+
+    # Save for later comparison
+    prev_q_proj_cols = torch.clone(spmha.q_proj.adapter.cols)
+    prev_q_proj_rows = torch.clone(spmha.q_proj.adapter.rows)
+    prev_k_proj_cols = torch.clone(spmha.k_proj.adapter.cols)
+    prev_k_proj_rows = torch.clone(spmha.k_proj.adapter.rows)
+    prev_v_proj_cols = torch.clone(spmha.v_proj.adapter.cols)
+    prev_v_proj_rows = torch.clone(spmha.v_proj.adapter.rows)
+    prev_out_proj_cols = torch.clone(spmha.out_proj.adapter.cols)
+    prev_out_proj_rows = torch.clone(spmha.out_proj.adapter.rows)
+
+    # Forwards are approx equal since adapters were initialized with near-zero values
+    mha_out, _ = mha.forward(x, x, x)
+    spmha_out, _ = spmha.forward(x, x, x)
+    assert torch.allclose(mha_out, spmha_out, atol=1e-3)
+
+    # Train a bit
+    mse = nn.MSELoss()
+    loss = mse(spmha_out, y)
+    loss.backward()
+    optimizer.step()
+
+    # MHA weight params remain unchanged
+    assert torch.equal(
+        mha.in_proj_weight,
+        torch.cat(
+            (
+                spmha.q_proj.weight,
+                spmha.k_proj.weight,
+                spmha.v_proj.weight,
+            )
+        ),
+    )
+    assert torch.equal(
+        mha.out_proj.weight,
+        spmha.out_proj.weight,
+    )
+
+    # Biases changed
+    assert not torch.equal(mha.in_proj_bias, spmha.in_proj_bias)
+    assert not torch.equal(mha.out_proj.bias, spmha.out_proj.bias)
+
+    # Adapter params changed
+    assert not torch.equal(prev_q_proj_cols, spmha.q_proj.adapter.cols)
+    assert not torch.equal(prev_q_proj_rows, spmha.q_proj.adapter.rows)
+    assert not torch.equal(prev_k_proj_cols, spmha.k_proj.adapter.cols)
+    assert not torch.equal(prev_k_proj_rows, spmha.k_proj.adapter.rows)
+    assert not torch.equal(prev_v_proj_cols, spmha.v_proj.adapter.cols)
+    assert not torch.equal(prev_v_proj_rows, spmha.v_proj.adapter.rows)
+    assert not torch.equal(prev_out_proj_cols, spmha.out_proj.adapter.cols)
+    assert not torch.equal(prev_out_proj_rows, spmha.out_proj.adapter.rows)
+
+    # Export to mha
+    mha2 = spmha.to_module()
+
+    # Weights and biases are equal
+    assert torch.equal(  # in_proj_weight property uses adapted_weights
+        mha2.in_proj_weight, spmha.in_proj_weight
+    )
+    assert torch.equal(mha2.in_proj_bias, spmha.in_proj_bias)
+    assert torch.equal(mha2.out_proj.weight, spmha.out_proj.adapted_weight)
+    assert torch.equal(mha2.out_proj.bias, spmha.out_proj.bias)
+
+
+def test_splora_mha_diff_qkv_dim():
+    rank = 2
+    embed_dim = 16
+    kdim = vdim = 24
+    num_heads = 4
+    seq_len = 8
+    batch_size = 2
+    q = torch.randn(batch_size, seq_len, embed_dim)
+    k = torch.randn(batch_size, seq_len, kdim)
+    v = torch.randn(batch_size, seq_len, vdim)
+    y = torch.randn(batch_size, seq_len, embed_dim)
+
+    mha = nn.MultiheadAttention(
+        embed_dim,
+        num_heads,
+        dropout=0.0,
+        bias=True,
+        add_bias_kv=True,
+        add_zero_attn=False,
+        kdim=kdim,
+        vdim=vdim,
+        batch_first=True,
+    )
+    mha.in_proj_bias = torch.nn.Parameter(torch.rand_like(mha.in_proj_bias))
+
+    # Transfer
+    spmha = SPLoRA(mha, rank)
+
+    # Weights are all close, biases are equal
+
+    assert torch.equal(mha.q_proj_weight, spmha.q_proj.weight)
+    assert torch.equal(mha.k_proj_weight, spmha.k_proj.weight)
+    assert torch.equal(mha.v_proj_weight, spmha.v_proj.weight)
+    assert torch.equal(mha.out_proj.weight, spmha.out_proj.weight)
+    assert torch.allclose(
+        mha.q_proj_weight, spmha.q_proj.adapted_weight, atol=_DEFAULT_INIT_RANGE
+    )
+    assert torch.allclose(
+        mha.k_proj_weight, spmha.k_proj.adapted_weight, atol=_DEFAULT_INIT_RANGE
+    )
+    assert torch.allclose(
+        mha.v_proj_weight, spmha.v_proj.adapted_weight, atol=_DEFAULT_INIT_RANGE
+    )
+    assert torch.allclose(
+        mha.out_proj.weight, spmha.out_proj.adapted_weight, atol=_DEFAULT_INIT_RANGE
+    )
+
+    assert torch.equal(mha.in_proj_bias, spmha.in_proj_bias)
+    assert torch.equal(mha.bias_k, spmha.bias_k)
+    assert torch.equal(mha.bias_v, spmha.bias_v)
+    assert torch.equal(mha.out_proj.bias, spmha.out_proj.bias)
+
+    # Prepare optim
+    optimizer = torch.optim.Adam(spmha.parameters())
+    optimizer.zero_grad()
+
+    # Save for later comparison
+    prev_q_proj_cols = torch.clone(spmha.q_proj.adapter.cols)
+    prev_q_proj_rows = torch.clone(spmha.q_proj.adapter.rows)
+    prev_k_proj_cols = torch.clone(spmha.k_proj.adapter.cols)
+    prev_k_proj_rows = torch.clone(spmha.k_proj.adapter.rows)
+    prev_v_proj_cols = torch.clone(spmha.v_proj.adapter.cols)
+    prev_v_proj_rows = torch.clone(spmha.v_proj.adapter.rows)
+    prev_out_proj_cols = torch.clone(spmha.out_proj.adapter.cols)
+    prev_out_proj_rows = torch.clone(spmha.out_proj.adapter.rows)
+
+    # Forwards are approx equal since adapters were initialized with near-zero values
+    mha_out, _ = mha.forward(q, k, v)
+    spmha_out, _ = spmha.forward(q, k, v)
+    assert torch.allclose(mha_out, spmha_out, atol=1e-3)
+
+    # Train a bit
+    mse = nn.MSELoss()
+    loss = mse(spmha_out, y)
+    loss.backward()
+    optimizer.step()
+
+    # MHA weight params remain unchanged
+    assert torch.equal(mha.q_proj_weight, spmha.q_proj.weight)
+    assert torch.equal(mha.k_proj_weight, spmha.k_proj.weight)
+    assert torch.equal(mha.v_proj_weight, spmha.v_proj.weight)
+    assert torch.equal(mha.out_proj.weight, spmha.out_proj.weight)
+
+    # Biases changed
+    assert not torch.equal(mha.in_proj_bias, spmha.in_proj_bias)
+    assert not torch.equal(mha.bias_k, spmha.bias_k)
+    assert not torch.equal(mha.bias_v, spmha.bias_v)
+    assert not torch.equal(mha.out_proj.bias, spmha.out_proj.bias)
+
+    # Adapter params changed
+    assert not torch.equal(prev_q_proj_cols, spmha.q_proj.adapter.cols)
+    assert not torch.equal(prev_q_proj_rows, spmha.q_proj.adapter.rows)
+    assert not torch.equal(prev_k_proj_cols, spmha.k_proj.adapter.cols)
+    assert not torch.equal(prev_k_proj_rows, spmha.k_proj.adapter.rows)
+    assert not torch.equal(prev_v_proj_cols, spmha.v_proj.adapter.cols)
+    assert not torch.equal(prev_v_proj_rows, spmha.v_proj.adapter.rows)
+    assert not torch.equal(prev_out_proj_cols, spmha.out_proj.adapter.cols)
+    assert not torch.equal(prev_out_proj_rows, spmha.out_proj.adapter.rows)
+
+    # Export to mha
+    mha2 = spmha.to_module()
+
+    # Weights and biases are equal
+    assert torch.equal(mha2.q_proj_weight, spmha.q_proj.adapted_weight)
+    assert torch.equal(mha2.k_proj_weight, spmha.k_proj.adapted_weight)
+    assert torch.equal(mha2.v_proj_weight, spmha.v_proj.adapted_weight)
+    assert torch.equal(mha2.out_proj.weight, spmha.out_proj.adapted_weight)
+
+    assert torch.equal(mha2.in_proj_bias, spmha.in_proj_bias)
+    assert torch.equal(mha2.bias_k, spmha.bias_k)
+    assert torch.equal(mha2.bias_v, spmha.bias_v)
+    assert torch.equal(mha2.out_proj.bias, spmha.out_proj.bias)
+
+
 def test_splora_named_parameters():
     in_features = 9
     out_features = 4
